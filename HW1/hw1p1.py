552345cf0e932e37742b3a08278de1472b053a00
dhupka <dhupka1@gmail.com>
2021-03-08 10:50:22 -0500

    big update

--- HW1/hw1p1.py
+++ HW1/hw1p1.py
@@ -1,25 +1,26 @@
 import torch
 import time
-
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 # Images
-img = 'intersection(nyc_street).jpg'
+#img = 'intersection(nyc_street).jpg'
+dir = 'https://github.com/ultralytics/yolov5/raw/master/data/images/'
+imgs = [dir + f for f in ('zidane.jpg', 'bus.jpg')]  # batched list of images
 
 # Inference
-#start = time.time()         #Beginning of measuring execution time
-results = model(img)
-#stop = time.time()          #Ending of measuring execution time
-#duration = stop - start
+start = time.time()         #Beginning of measuring execution time
+results = model(imgs)
+stop = time.time()          #Ending of measuring execution time
+duration = stop - start
 
 # Results
 results.print()
 results.show()  # or .show()
-#print(duration)
+print(duration)
 
 # Data
-#print(results.xyxy[0])  # print img1 predictions (pixels)
+print(results.xyxy[0])  # print img1 predictions (pixels)
 #                   x1           y1           x2           y2   confidence        class
 # tensor([[7.50637e+02, 4.37279e+01, 1.15887e+03, 7.08682e+02, 8.18137e-01, 0.00000e+00],
 #         [9.33597e+01, 2.07387e+02, 1.04737e+03, 7.10224e+02, 5.78011e-01, 0.00000e+00],
---
+++ HW1/hw1p2.py
@@ -0,0 +1,32 @@
+import torch
+import time
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True)
+
+# Images
+dir = 'input/'
+imgs1 = [dir + ('firetruck.jpeg')]
+imgs8 = [dir + f for f in ('ac.jpeg', 'bicycle.jpeg', 'bread.jpeg', 'bus.jpeg', 
+ 'candle.jpeg', 'computer.jpeg', 'dogs.jpeg', 'eggs.jpeg')]  
+imgs16 = [dir + f for f in ('ac.jpeg', 'bicycle.jpeg', 'bread.jpeg', 'bus.jpeg', 
+ 'candle.jpeg', 'computer.jpeg', 'dogs.jpeg', 'eggs.jpeg', 'firetruck.jpeg', 'pencil.jpg',
+ 'salad.jpeg', 'soap.jpeg', 'toothbrush.jpeg', 'wagon.jpeg', 'table.jpeg')] 
+
+# Inference
+start = time.time()         #Beginning of measuring execution time
+results = model(imgs16)
+stop = time.time()          #Ending of measuring execution time
+duration = stop - start
+
+# Results
+results.print()
+#results.show()  # or .show()
+print(duration)
+
+# Data
+#print(results.xyxy[0])  # print img1 predictions (pixels)
+#                   x1           y1           x2           y2   confidence        class
+# tensor([[7.50637e+02, 4.37279e+01, 1.15887e+03, 7.08682e+02, 8.18137e-01, 0.00000e+00],
+#         [9.33597e+01, 2.07387e+02, 1.04737e+03, 7.10224e+02, 5.78011e-01, 0.00000e+00],
+#         [4.24503e+02, 4.29092e+02, 5.16300e+02, 7.16425e+02, 5.68713e-01, 2.70000e+01]])
